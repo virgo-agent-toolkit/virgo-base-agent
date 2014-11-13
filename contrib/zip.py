492560c6bc90c2e79d5d9dcc293f9a7eafa5094c
Ryan Phillips <ryan.phillips@rackspace.com>
2014-11-13 15:39:13 -0600

    cleanup

---
+++ contrib/zip.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import os
+import sys
+import zipfile
+
+class FileCtx:
+    def __init__(self, relpath, realpath):
+        self.relpath = relpath
+        self.realpath = realpath
+
+class VirgoZip(zipfile.ZipFile):
+    def __init__(self, roots, out):
+        zipfile.ZipFile.__init__(self, out, 'w', zipfile.ZIP_DEFLATED)
+        self.roots = roots
+        self.files = {}
+
+    def run(self):
+        for root in self.roots:
+            for r, dirs, files in os.walk(root):
+                for f in files:
+                    realpath = os.path.join(r, f)
+                    relpath = os.path.relpath(realpath, root)
+                    if relpath:
+                        fctx = FileCtx(relpath, realpath)
+                        self.files[fctx.relpath] = fctx
+
+    def _write(self, ctx):
+        print(ctx.relpath)
+        self.write(ctx.realpath, ctx.relpath) 
+
+    def save(self):
+        [ self._write(ctx) for _, ctx in sorted(self.files.iteritems()) ]
+
+z = VirgoZip(sys.argv[2:], sys.argv[1])
+z.run()
+z.save()
+z.close()
