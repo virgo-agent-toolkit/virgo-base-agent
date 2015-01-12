62bbee9f3800fa94fd9eaf1b7fe4c2363f126bba
Ryan Phillips <ryan.phillips@rackspace.com>
2015-01-12 12:05:03 -0600

    change to rphillips/logging

--- package.lua
+++ package.lua
@@ -6,6 +6,6 @@ return {
     "rphillips/hsm@0.0.2",
     "rphillips/request@0.0.3",
     "rphillips/split-stream@0.1.0",
-    "rphillips/virgo-logging@0.1.0",
+    "rphillips/logging@0.1.0",
   },
 }
