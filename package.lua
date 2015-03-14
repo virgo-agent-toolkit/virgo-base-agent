6e01145ae7ec5d1d8bc4908aa41f7738b24b1839
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-13 19:19:07 -0500

    change request dependency

--- package.lua
+++ package.lua
@@ -1,14 +1,14 @@
 return {
   name = "virgo-agent-toolkit/virgo",
-  version = "0.11.0",
+  version = "0.11.1",
   dependencies = {
     "luvit/luvit@1.9.5",
     "rphillips/async@0.0.2",
     "rphillips/hsm@0.0.2",
     "rphillips/line-emitter@0.3.3",
     "rphillips/logging@0.1.3",
-    "rphillips/request@0.0.3",
     "rphillips/split-stream@0.4.0",
+    "virgo-agent-toolkit/request@0.2.2",
   },
   files = {
     "**.lua",
