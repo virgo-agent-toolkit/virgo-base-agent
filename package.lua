536b00eb5b691ae443529d50a525b09b014ee47b
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-13 19:20:03 -0500

    bump to luvit 2.0.1

--- package.lua
+++ package.lua
@@ -1,8 +1,8 @@
 return {
   name = "virgo-agent-toolkit/virgo",
-  version = "0.11.1",
+  version = "0.11.2",
   dependencies = {
-    "luvit/luvit@1.9.5",
+    "luvit/luvit@2.0.1",
     "rphillips/async@0.0.2",
     "rphillips/hsm@0.0.2",
     "rphillips/line-emitter@0.3.3",
