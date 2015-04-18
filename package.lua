8a5a5f9d7f315e5817f85f6a78c147afaff1fa6d
Ryan Phillips <ryan.phillips@rackspace.com>
2015-04-18 14:09:19 -0500

    bump to 0.13.1

--- package.lua
+++ package.lua
@@ -1,15 +1,15 @@
 return {
   name = "virgo-agent-toolkit/virgo",
-  version = "0.13.0",
+  version = "0.13.1",
   dependencies = {
     "luvit/luvit@2.0.6",
     "creationix/semver@1.0.1",
-    "rphillips/async@0.0.2",
     "rphillips/hsm@0.0.2",
     "rphillips/logging@0.1.4",
+    "virgo-agent-toolkit/async@1",
     "virgo-agent-toolkit/line-emitter@0.5.0",
     "virgo-agent-toolkit/split-stream@0.5.3",
-    "virgo-agent-toolkit/request@0.2.3",
+    "virgo-agent-toolkit/request@1"
   },
   files = {
     "**.lua",
