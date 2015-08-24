bf90f4f5ede99f2447e0332584d09bf6274f1ea0
Ryan Phillips <ryan.phillips@rackspace.com>
2015-08-24 14:31:09 -0500

    resurrect PR #96

--- machineidentity.lua
+++ machineidentity.lua
@@ -14,17 +14,37 @@ See the License for the specific language governing permissions and
 limitations under the License.
 --]]
 
+local async = require('async')
 local Error = require('core').Error
 local Object = require('core').Object
 local childprocess = require('childprocess')
-local los = require('los')
-local fs = require('fs')
-local utils = require('./utils')
 local fireOnce = require('./util/misc').fireOnce
 local fmt = require('string').format
+local fs = require('fs')
+local http = require('http')
+local los = require('los')
 local table = require('table')
+local utils = require('./utils')
 
 local MachineIdentity = Object:extend()
+
+local function awsAdapter(callback)
+  local uri = 'http://instance-data.ec2.internal/latest/meta-data/instance-id'
+  local req = http.request(uri, function(res)
+    local id = ''
+    res:on('data', function(data)
+      id = id .. data
+    end)
+    res:on("end", function()
+      res:destroy()
+      callback(nil, id)
+    end)
+  end)
+  req:setTimeout(2000)
+  req:once('timeout', callback)
+  req:once('error', callback)
+  req:done()
+end
 
 local function xenAdapter(callback)
   local exePath, exeArgs
@@ -111,32 +131,29 @@ function MachineIdentity:initialize(config)
 end
 
 function MachineIdentity:get(callback)
-  local rv, handle_id, onCloudInit, onXenAdapter
+  local instanceId
 
-  rv = utils.tableGetBoolean(self._config, 'autodetect_machine_id', true)
-  if rv == false then
-    return callback()
-  end
+  local rv = utils.tableGetBoolean(self._config, 'autodetect_machine_id', true)
+  if not rv then return callback() end
 
-  function handle_id(instanceId)
-    callback(nil, {id = instanceId})
-  end
+  local adapters = {
+    cloudInitAdapter,
+    xenAdapter,
+    awsAdapter
+  }
 
-  function onXenAdapter(err, instanceId)
-    if err then
-      return callback(err)
-    end
-    handle_id(instanceId)
-  end
-
-  function onCloudInit(err, instanceId)
-    if err then
-      return xenAdapter(onXenAdapter)
-    end
-    handle_id(instanceId)
-  end
-
-  cloudInitAdapter(onCloudInit)
+  async.forEachSeries(adapters, function(adapter, callback)
+    adapter(function(err, _instanceId)
+      if err then return callback() end
+      if instanceId then return callback() end
+      instanceId = _instanceId
+      callback()
+    end)
+  end, function(err)
+    if err then return callback(err) end
+    if not instanceId then return callback(Error:new('no instance id')) end
+    callback(nil, { id = instanceId })
+  end)
 end
 
 exports.MachineIdentity = MachineIdentity
