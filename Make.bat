3092509118614b0f9cfc62a85c0ec0a5255b3b53
Ryan Phillips <ryan.phillips@rackspace.com>
2015-08-26 16:22:54 -0500

    use luvisigar target

--- Make.bat
+++ Make.bat
@@ -8,10 +8,14 @@ ECHO "Building lit"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/luvit/lit/%LIT_VERSION%/get-lit.ps1'))"
 GOTO :end
 
+:luvisigar
+CALL lit.exe get-luvi -o luvi-sigar.exe
+GOTO :end
+
 :test
 IF NOT EXIST lit.exe CALL Make.bat lit
+IF NOT EXIST luvi-sigar.exe CALL Make.bat luvisigar
 CALL lit.exe install
-CALL lit.exe get-luvi -o luvi-sigar.exe
 CALL luvi-sigar.exe . -m tests\run.lua
 GOTO :end
 
